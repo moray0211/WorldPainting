eea346f0cc6745957dbe6331832c8fe2e0f2dea1
jeongopo <jeong_opo@naver.com>
2021-05-27 23:18:32 +0900

    0527 남현정 작업
    1. DialogueTrigger
    : 아이템 사용과 대화 스크립트의 중복 방지를 위해.
    2. UI-DialgoueCanvas-ObjectBarrierPanel 추가
    : 대화중일 시 오브젝트에 마우스 입력이 들어가지 않도록 하기 위함
    3. Item : 아이템 상호작용 추가

--- Assets/Scripts/Item/ItemInteractable.cs
+++ Assets/Scripts/Item/ItemInteractable.cs
@@ -14,10 +14,12 @@ public class ItemInteractable : MonoBehaviour
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D rayhit = Physics2D.Raycast(mousePos, Vector2.zero);
-            if (rayhit.collider != null && rayhit.transform == this.gameObject.transform)
+            if (rayhit.collider != null && rayhit.transform == this.gameObject.transform&&!(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()))
             {
-                if (GameObject.Find("DialogueManager").GetComponent<DialogueManager>().inConversation == false)
+                if (GameObject.Find("DialogueManager").GetComponent<DialogueManager>().inConversation == false){
+                    Debug.Log("클릭");
                     Interact();
+                }
             }
         }
 
--- Assets/Scripts/Item/ItemInteractionObject.cs
+++ Assets/Scripts/Item/ItemInteractionObject.cs
@@ -3,9 +3,14 @@
 public class ItemInteractionObject : ItemInteractable
 {
 
-    public Item interactableItem; //상호작용할 아이템
+    public Item interactableItem; //상호작용할 아이템들
     public bool destroyObject = false; //사용시 오브젝트 사라짐 유무
     public bool destroyItem = false; //사용시 아이템 사라짐 유무
+
+    public bool isTriggerDiaglogue=false; //대화를 진행할 것인지
+    public Dialogue dialogue; //대화 진행한다면 그 내용
+    public Switch[] reqSwitch; //아이템을 사용하기 위한 스위치
+    public Switch[] onSwitchAfterInteract; //Interact 이후 On되는 스위치
 
     [TextArea(3, 10)]
     public string comment = "상호작용 설명 주석";
@@ -13,8 +18,18 @@ public class ItemInteractionObject : ItemInteractable
     public override void Interact()
     {
         base.Interact();
+        bool allReqSwitchOn = true;
 
-        InteractItem();
+        if(reqSwitch!=null) {
+             for (int i = 0; i < reqSwitch.Length; i++)
+            {
+                if (!reqSwitch[i].isSwitchActive)
+                {
+                    allReqSwitchOn = false;
+                }
+            }
+            if (allReqSwitchOn) InteractItem();
+        }else InteractItem();
     }
 
     void InteractItem()
@@ -24,7 +39,6 @@ public class ItemInteractionObject : ItemInteractable
   
         for (int i = 0; i < inventorySlots.Length; i++)
         {
-
             if (inventorySlots[i].getItem() != null && inventorySlots[i].getItem() == interactableItem && inventorySlots[i].getItem().getItemActive())
             {
                 //상호작용
@@ -32,10 +46,24 @@ public class ItemInteractionObject : ItemInteractable
                 inventorySlots[i].GetComponent<InventorySlotItemActive>().CancleAllSlotsActive();
                 if (destroyItem) Inventory.instance.Remove(interactableItem);
                 if (destroyObject) Destroy(gameObject);
+                if (onSwitchAfterInteract.Length!=0){
+                    for(int j=0;j<onSwitchAfterInteract.Length;j++){
+                        onSwitchAfterInteract[j].setSwitchActive(true);
+                        //상호작용 후 켜줄 스위치가 있다면 On해주기
+                    }
+                } 
                 Object.FindObjectOfType<InventorySlotItemActive>().CancleAllSlotsActive();
-                return;
-            }
 
+                if(isTriggerDiaglogue){ //아이템 사용시 대화한다면
+                    FindObjectOfType<DialogueManager>().dialogueCanvas.SetActive(true); //대화창 호출
+                    FindObjectOfType<DialogueManager>().StartDialogue(dialogue, new DialogueTrigger.SwitchOnOffInf());
+                }
+        
+                return;
+            
+            
+
+            }
         }
         Object.FindObjectOfType<InventorySlotItemActive>().CancleAllSlotsActive();
     }
